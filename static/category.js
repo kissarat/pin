5cb67f1889a2b2be271ba803205cba4c08cbff6b
nikluz <nnikluz@gmail.com>
2014-05-30 11:06:23 +0300

    Pin wheel on repin.

--- static/category.js
+++ static/category.js
@@ -243,6 +243,7 @@ jQuery(function() {
       show_error($(this).find('#board_creation_layer'), 'Please select or add a list');
     }
     if (form_has_errors) {
+      $("body").removeClass("loading");
       return false;
     }
     return true;
