1dd26fb001ea5af84ffb2decd95dc834bedb9fee
nikluz <nnikluz@gmail.com>
2014-06-06 18:20:48 +0300

    Fixed sql for remove custom boards. Modified serializer

--- config/db/db_boards_cleanup.sql
+++ config/db/db_boards_cleanup.sql
@@ -2,14 +2,14 @@
 DELETE from pins_categories
        where pin_id IN
        (SELECT id from pins where board_id IN
-       (SELECT id from BOARDS where name <> 'Things to get'
-       AND name <> 'Places to visit' AND name <> 'Food to eat'));
+       (SELECT id from BOARDS where (name <> 'Things to get'
+       AND name <> 'Places to visit' AND name <> 'Food to eat') OR name IS NULL));
 -- Deleting pins related to removed boards
 DELETE from pins where board_id IN
-       (SELECT id from BOARDS where name <> 'Things to get'
+       (SELECT id from BOARDS where (name <> 'Things to get'
        AND name <> 'Places to visit'
-       AND name <> 'Food to eat');
+       AND name <> 'Food to eat') OR name IS NULL);
 -- Deleting removed boards themselves
-DELETE from BOARDS where name <> 'Things to get'
+DELETE from BOARDS where (name <> 'Things to get'
        AND name <> 'Places to visit'
-       AND name <> 'Food to eat';
+       AND name <> 'Food to eat') OR name IS NULL;
