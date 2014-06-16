61c0b8738b1fe2d5a479bf4e9f46685f364a3c98
Camilo Torres <camilotorresf@gmail.com>
2014-06-16 10:01:41 -0430

    fix search any for pins search

--- mypinnings/admin/pins.py
+++ mypinnings/admin/pins.py
@@ -82,15 +82,23 @@ class SearchPagination(object):
     def find_within_a_category(self):
         db = database.get_db()
         if self.category == 'uncategorized':
-            query_where = 'pins.id not in (select pin_id from pins_categories)'
+            results = db.select(tables='pins',
+                               what='distinct pins.*',
+                               where='pins.id not in (select pin_id from pins_categories)',
+                               order='{} {}'.format(self.sort, self.sort_direction),
+                               vars={'catid': self.category},
+                               limit=PAGE_SIZE, offset=(PAGE_SIZE * self.page))
+        elif self.category == 'any':
+            results = db.select(tables=['pins'],
+                               order='{} {}'.format(self.sort, self.sort_direction),
+                               limit=PAGE_SIZE, offset=(PAGE_SIZE * self.page))
         else:
-            query_where = 'pins.id=pins_categories.pin_id and pins_categories.category_id=$catid'
-        results = db.select(tables=['pins, pins_categories'],
-                           what='distinct pins.*',
-                           where=query_where,
-                           order='{} {}'.format(self.sort, self.sort_direction),
-                           vars={'catid': self.category},
-                           limit=PAGE_SIZE, offset=(PAGE_SIZE * self.page))
+            results = db.select(tables=['pins, pins_categories'],
+                               what='distinct pins.*',
+                               where='pins.id=pins_categories.pin_id and pins_categories.category_id=$catid',
+                               order='{} {}'.format(self.sort, self.sort_direction),
+                               vars={'catid': self.category},
+                               limit=PAGE_SIZE, offset=(PAGE_SIZE * self.page))
         pins = []
         for row in results:
             pins.append(row)
