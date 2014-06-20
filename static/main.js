bce3c42c3962581302e4377b028d261744964db0
Oleg Tarasenko <oltarasenko@gmail.com>
2014-06-20 12:04:13 +0300

    Fixed Change Background button on profileheader

--- static/main.js
+++ static/main.js
@@ -81,19 +81,94 @@ $(document).ready(function() {
             $elem.text(data);
         });
     });
-});
 
+    var init_bg_pos;
+    $("li > #change_background").click(function(e){
+        $(".profCoverBackground").css( "z-index", "0" );
+        $("#header_background").css( "z-index", "100" );
+        $("#save_background").text("Save position");
+        $("#save_background").css("display","block");
+        $("#cancel_background_move").css("display","block");
+        $("#instructionWrap").show();
+        $("#transbox").css("cursor","move");
 
-function hide_pins_date() {
-  $(".dateWrap").each(function() {
-    if($('.dwContent .dwItem:not(.pin-not-added)', this).length == 0) {
-      $(this).hide();
+        init_bg_pos = $("#header_background").css("background-position");
+    });
+
+    function save_bg() {
+      var tempX, tempY, _ref;
+
+      _ref = $("#header_background").css("background-position").split(" ");
+      otherX = _ref[0];
+      otherY = _ref[1];
+      tempX = parseInt(otherX.slice(0, +(otherX.length - 2) + 1 || 9e9));
+      tempY = parseInt(otherY.slice(0, +(otherY.length - 2) + 1 || 9e9));
+      return $.ajax({
+        url: "/changebgpos",
+        data: {
+        x: tempX,
+        y: tempY },
+        success: function(){},
+          beforeSend: function(){
+          $("body").removeClass('loading');
+        },
+        type: 'POST',
+      });
     }
-  });
-}
 
-function show_pins_date() {
-  $(".dateWrap").each(function() {
-    $(this).show();
-  });
-}
+    $("#save_background").click(function(e){
+        var action = $("#save_background").text();
+        $("#save_background").css("display","none");
+        $("#cancel_background_move").css("display","none");
+        if (action == "Save position") {
+            $(".profCoverBackground").css( "z-index", "2" );
+            $("#header_background").css( "z-index", "" );
+            $("#instructionWrap").hide();
+            $("#transbox").css("cursor","default");
+            save_bg();
+        }
+    });
+
+    $("#cancel_background_move").click(function(e){
+        $("#header_background").css("background-position", init_bg_pos);
+        $(".profCoverBackground").css( "z-index", "2" );
+        $("#header_background").css( "z-index", "" );
+        $("#instructionWrap").hide();
+        $("#transbox").css("cursor","default");
+        $("#save_background").css("display","none");
+        $("#cancel_background_move").css("display","none");
+    });
+
+
+
+    $("#dropdown_bg > ul").mouseleave(function(e){
+        $("#dropdown_bg").removeClass("open");
+    });
+
+    $("#dropdown_bg").mouseenter(function(e){
+        $("#dropdown_bg").css("display","block");
+    });
+
+    $(".profCoverBackground").on( "mouseover", function() {
+        $("#dropdown_bg").css({
+            "display": "block"
+        });
+    }).on( "mouseleave", function() {
+        $("#dropdown_bg").css({ "display": "none" });
+    });
+
+
+    function hide_pins_date() {
+        $(".dateWrap").each(function() {
+            if($('.dwContent .dwItem:not(.pin-not-added)', this).length == 0) {
+                $(this).hide();
+            }
+        });
+    }
+
+    function show_pins_date() {
+        $(".dateWrap").each(function() {
+            $(this).show();
+        });
+    }
+});
