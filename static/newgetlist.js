9e8abcbbbeb77d1899f16369a94f67ef08bdf29c
oakoval <koval.books@gmail.com>
2014-06-22 23:49:23 +0300

    Fixed image unavailable

--- static/newgetlist.js
+++ static/newgetlist.js
@@ -154,9 +154,6 @@ $(document).ready(function() {
         gallery.prev();
     });
 
-    $('#fetch-images, #fetch-images-profile').click(function(){
-    var url_input_to_fetch = $(this).parent().find(".fetch-url");
-
     function show_fetch_images_error(error) {
         $("#statusweb").html(error);
         $(".block-loading").hide();
@@ -164,37 +161,54 @@ $(document).ready(function() {
         return false;
     }
 
-    var url = url_input_to_fetch.val();
-    if (/(\.jpg|\.jpeg|\.png|\.gif)$/.test(url))
-        initgallery([url]);
-    else $.ajax({
-        url:"/preview",
-        dataType: 'json',
-        data: {
-            url: url
-        },
+    $('#fetch-images, #fetch-images-profile').click(function(){
+        var url_input_to_fetch = $(this).parent().find(".fetch-url");
 
-        beforeSend: function() {
-            $("#getlist-from-web").dialog("close");
-            $(".block-loading").show();
-        },
+        var url = url_input_to_fetch.val();
+        if (is_image_url(url))
+            initgallery([url]);
+        else {
+            var request = $.ajax({
+                url: "/preview",
+                dataType: 'json',
+                timeout: 80000,
+                cache: true,
+                data: { url: url },
 
-        success: function(data) {
-            if ('error' == data.status)
-                return show_fetch_images_error(data.error);
+                beforeSend: function() {
+                    $("#getlist-from-web").dialog("close");
+                    $(".block-loading").show();
+                },
+
+                success: function(data) {
+                    if ('error' == data.status)
+                        return show_fetch_images_error(data.error);
 //                $("#statusweb").html("please provide a valid image url");
-            if (1 != data.images.length)
-                $("#websitelinkweb").val(url_input_to_fetch.val());
-            if (!$('#titleweb').val())
-                $('#titleweb').val(data.title);
-            $("#getlist-from-web").clearForm();
-            initgallery(data.images);
-        },
+                    if (1 != data.images.length)
+                        $("#websitelinkweb").val(url_input_to_fetch.val());
+                    if (!$('#titleweb').val())
+                        $('#titleweb').val(data.title);
+                    $("#getlist-from-web").clearForm();
+                    $(".block-loading").hide();
+                    $("#addpindialogformweb").dialog("open");
+                    initgallery(data.images);
+                },
 
-	    error: function(xhr, status, error) {
-            show_fetch_images_error(status  + ' ' + error);
-	    }
-    })
+                error: function(xhr, status, error) {
+                    show_fetch_images_error(error);
+                },
+
+                complete: function(xhr, status) {
+                    console.log('preview ' + status + ' ' + url);
+                    //show_fetch_images_error(status);
+                }
+            });
+            $(window).keyup(function() {
+                $(window).unbind('keyup', arguments.callee);
+                request.abort();
+                request = null;
+            });
+        }
     });
 
     function initgallery(data){
@@ -209,7 +223,6 @@ $(document).ready(function() {
         }
 
         //gallery.init();
-
     }
 
     function getdata() {
@@ -503,6 +516,10 @@ function load_image_from_url(image, url, title) {
     $('#fetch-images').click();
 }
 
+function is_image_url(url) {
+    return /(\.jpg|\.jpeg|\.png|\.gif)$/.test(url);
+}
+
 var websearch = {
     offset: 0,
     buffer: [],
@@ -521,13 +538,15 @@ var websearch = {
                 .attr('src', result.thumb)
                 .attr('data-src', result.image)
                 .load(function() {
+                    if (!is_image_url(this.getAttribute('data-src')))
+                        return;
                     var img = new Image();
                     img.src = this.getAttribute('data-src');
                     var self = this;
                     img.onload = function() {
                         self.src = this.src;
                     };
-                    img.onerror = function(cell) {
+                    img.onerror = function() {
                         self.src = '/static/img/unavailable.png';
                         $(self.parentNode)
                             .addClass('unavailable')
@@ -588,7 +607,7 @@ function decodeHTMLEntities(str) {
 	});
 	str = str.replace(/(&\w+;)/g, function(entity) {
 		entity = entity.slice(1, -1);
-		var entityCode = htmlEntityCodes[entity];
+		var entityCode = HtmlEntityCodes[entity];
 		if ('number' != typeof entityCode) {
 			console.error('Invalid HTML entity &' + entity + ';');
 			return '';
@@ -598,4 +617,9 @@ function decodeHTMLEntities(str) {
 	return str;
 }
 
-var htmlEntityCodes = {quot:34,amp:38,apos:39,lt:60,gt:62,nbsp:160,iexcl:161,cent:162,pound:163,curren:164,yen:165,brvbar:166,sect:167,uml:168,copy:169,ordf:170,laquo:171,not:172,shy:173,reg:174,macr:175,deg:176,plusmn:177,sup2:178,sup3:179,acute:180,micro:181,para:182,middot:183,cedil:184,sup1:185,ordm:186,raquo:187,frac14:188,frac12:189,frac34:190,iquest:191,Agrave:192,Aacute:193,Acirc:194,Atilde:195,Auml:196,Aring:197,AElig:198,Ccedil:199,Egrave:200,Eacute:201,Ecirc:202,Euml:203,Igrave:204,Iacute:205,Icirc:206,Iuml:207,ETH:208,Ntilde:209,Ograve:210,Oacute:211,Ocirc:212,Otilde:213,Ouml:214,times:215,Oslash:216,Ugrave:217,Uacute:218,Ucirc:219,Uuml:220,Yacute:221,THORN:222,szlig:223,agrave:224,aacute:225,acirc:226,atilde:227,auml:228,aring:229,aelig:230,ccedil:231,egrave:232,eacute:233,ecirc:234,euml:235,igrave:236,iacute:237,icirc:238,iuml:239,eth:240,ntilde:241,ograve:242,oacute:243,ocirc:244,otilde:245,ouml:246,divide:247,oslash:248,ugrave:249,uacute:250,ucirc:251,uuml:252,yacute:253,thorn:254,yuml:255,OElig:338,oelig:339,Scaron:352,scaron:353,Yuml:376,fnof:402,circ:710,tilde:732,Alpha:913,Beta:914,Gamma:915,Delta:916,Epsilon:917,Zeta:918,Eta:919,Theta:920,Iota:921,Kappa:922,Lambda:923,Mu:924,Nu:925,Xi:926,Omicron:927,Pi:928,Rho:929,Sigma:931,Tau:932,Upsilon:933,Phi:934,Chi:935,Psi:936,Omega:937,alpha:945,beta:946,gamma:947,delta:948,epsilon:949,zeta:950,eta:951,theta:952,iota:953,kappa:954,lambda:955,mu:956,nu:957,xi:958,omicron:959,pi:960,rho:961,sigmaf:962,sigma:963,tau:964,upsilon:965,phi:966,chi:967,psi:968,omega:969,thetasym:977,upsih:978,piv:982,ensp:8194,emsp:8195,thinsp:8201,zwnj:8204,zwj:8205,lrm:8206,rlm:8207,ndash:8211,mdash:8212,lsquo:8216,rsquo:8217,sbquo:8218,ldquo:8220,rdquo:8221,bdquo:8222,dagger:8224,Dagger:8225,bull:8226,hellip:8230,permil:8240,prime:8242,Prime:8243,lsaquo:8249,rsaquo:8250,oline:8254,frasl:8260,euro:8364,image:8465,weierp:8472,real:8476,trade:8482,alefsym:8501,larr:8592,uarr:8593,rarr:8594,darr:8595,harr:8596,crarr:8629,lArr:8656,uArr:8657,rArr:8658,dArr:8659,hArr:8660,forall:8704,part:8706,exist:8707,empty:8709,nabla:8711,isin:8712,notin:8713,ni:8715,prod:8719,sum:8721,minus:8722,lowast:8727,radic:8730,prop:8733,infin:8734,ang:8736,and:8743,or:8744,cap:8745,cup:8746,"int":8747,there4:8756,sim:8764,cong:8773,asymp:8776,ne:8800,equiv:8801,le:8804,ge:8805,sub:8834,sup:8835,nsub:8836,sube:8838,supe:8839,oplus:8853,otimes:8855,perp:8869,sdot:8901,lceil:8968,rceil:8969,lfloor:8970,rfloor:8971,lang:9001,rang:9002,loz:9674,spades:9824,clubs:9827,hearts:9829,diams:9830};
+var HtmlEntityCodes = {quot:34,amp:38,apos:39,lt:60,gt:62,nbsp:160,iexcl:161,cent:162,pound:163,curren:164,yen:165,brvbar:166,sect:167,uml:168,copy:169,ordf:170,laquo:171,not:172,shy:173,reg:174,macr:175,deg:176,plusmn:177,sup2:178,sup3:179,acute:180,micro:181,para:182,middot:183,cedil:184,sup1:185,ordm:186,raquo:187,frac14:188,frac12:189,frac34:190,iquest:191,Agrave:192,Aacute:193,Acirc:194,Atilde:195,Auml:196,Aring:197,AElig:198,Ccedil:199,Egrave:200,Eacute:201,Ecirc:202,Euml:203,Igrave:204,Iacute:205,Icirc:206,Iuml:207,ETH:208,Ntilde:209,Ograve:210,Oacute:211,Ocirc:212,Otilde:213,Ouml:214,times:215,Oslash:216,Ugrave:217,Uacute:218,Ucirc:219,Uuml:220,Yacute:221,THORN:222,szlig:223,agrave:224,aacute:225,acirc:226,atilde:227,auml:228,aring:229,aelig:230,ccedil:231,egrave:232,eacute:233,ecirc:234,euml:235,igrave:236,iacute:237,icirc:238,iuml:239,eth:240,ntilde:241,ograve:242,oacute:243,ocirc:244,otilde:245,ouml:246,divide:247,oslash:248,ugrave:249,uacute:250,ucirc:251,uuml:252,yacute:253,thorn:254,yuml:255,OElig:338,oelig:339,Scaron:352,scaron:353,Yuml:376,fnof:402,circ:710,tilde:732,Alpha:913,Beta:914,Gamma:915,Delta:916,Epsilon:917,Zeta:918,Eta:919,Theta:920,Iota:921,Kappa:922,Lambda:923,Mu:924,Nu:925,Xi:926,Omicron:927,Pi:928,Rho:929,Sigma:931,Tau:932,Upsilon:933,Phi:934,Chi:935,Psi:936,Omega:937,alpha:945,beta:946,gamma:947,delta:948,epsilon:949,zeta:950,eta:951,theta:952,iota:953,kappa:954,lambda:955,mu:956,nu:957,xi:958,omicron:959,pi:960,rho:961,sigmaf:962,sigma:963,tau:964,upsilon:965,phi:966,chi:967,psi:968,omega:969,thetasym:977,upsih:978,piv:982,ensp:8194,emsp:8195,thinsp:8201,zwnj:8204,zwj:8205,lrm:8206,rlm:8207,ndash:8211,mdash:8212,lsquo:8216,rsquo:8217,sbquo:8218,ldquo:8220,rdquo:8221,bdquo:8222,dagger:8224,Dagger:8225,bull:8226,hellip:8230,permil:8240,prime:8242,Prime:8243,lsaquo:8249,rsaquo:8250,oline:8254,frasl:8260,euro:8364,image:8465,weierp:8472,real:8476,trade:8482,alefsym:8501,larr:8592,uarr:8593,rarr:8594,darr:8595,harr:8596,crarr:8629,lArr:8656,uArr:8657,rArr:8658,dArr:8659,hArr:8660,forall:8704,part:8706,exist:8707,empty:8709,nabla:8711,isin:8712,notin:8713,ni:8715,prod:8719,sum:8721,minus:8722,lowast:8727,radic:8730,prop:8733,infin:8734,ang:8736,and:8743,or:8744,cap:8745,cup:8746,"int":8747,there4:8756,sim:8764,cong:8773,asymp:8776,ne:8800,equiv:8801,le:8804,ge:8805,sub:8834,sup:8835,nsub:8836,sube:8838,supe:8839,oplus:8853,otimes:8855,perp:8869,sdot:8901,lceil:8968,rceil:8969,lfloor:8970,rfloor:8971,lang:9001,rang:9002,loz:9674,spades:9824,clubs:9827,hearts:9829,diams:9830};
+var KeyCodes = {STRG:17,CTRL:17,CTRLRIGHT:18,CTRLR:18,SHIFT:16,RETURN:13,ENTER:13,BACKSPACE:8,BCKSP:8,ALT:18,ALTR:17,ALTRIGHT:17,SPACE:32,WIN:91,MAC:91,FN:null,UP:38,DOWN:40,LEFT:37,RIGHT:39,ESC:27,DEL:46,F1:112,F2:113,F3:114,F4:115,F5:116,F6:117,F7:118,F8:119,F9:120,F10:121,F11:122,F12:123,backspace:8,tab:9,enter:13,shift:16,ctrl:17,alt:18,pause_break:19,caps_lock:20,escape:27,page_up:33,page_down:34,end:35,home:36,left_arrow:37,up_arrow:38,right_arrow:39,down_arrow:40,insert:45,"delete":46,0:48,1:49,2:50,3:51,4:52,5:53,6:54,7:55,8:56,9:57,a:65,b:66,c:67,d:68,e:69,f:70,g:71,h:72,i:73,j:74,k:75,l:76,m:77,n:78,o:79,p:80,q:81,r:82,s:83,t:84,u:85,v:86,w:87,x:88,y:89,z:90,left_window:91,right_window:92,select_key:93,numpad_0:96,numpad_1:97,numpad_2:98,numpad_3:99,numpad_4:100,numpad_5:101,numpad_6:102,numpad_7:103,numpad_8:104,numpad_9:105,multiply:106,add:107,subtract:109,decimal_point:110,divide:111,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123,num_lock:144,scroll_lock:145,semi_colon:186,equal_sign:187,comma:188,dash:189,period:190,forward_slash:191,grave_accent:192,open_bracket:219,backslash:220,closebracket:221,single_quote:222}
+if(Object.freeze) {
+    Object.freeze(HtmlEntityCodes);
+    Object.freeze(KeyCodes);
+}
